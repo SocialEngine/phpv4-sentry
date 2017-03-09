55a145c4bf9e4e96e15f4a0435ae87bf004e2705
RaymondBenc <me@raybenc.com>
2017-03-10 00:50:17 +0100

    Due to error handling, this required a shutdown callback

--- application/modules/Sentry/Service/Writer.php
+++ application/modules/Sentry/Service/Writer.php
@@ -4,6 +4,8 @@ class Module_Sentry_Service_Writer extends Zend_Log_Writer_Abstract
 {
     private static $sentry;
 
+    public static $message;
+
     public function __construct($config)
     {
         if (!self::$sentry) {
@@ -14,8 +16,8 @@ class Module_Sentry_Service_Writer extends Zend_Log_Writer_Abstract
             self::$sentry = new Raven_Client($config['dsn']);
             $errorHandler = new Raven_ErrorHandler(self::$sentry);
             $errorHandler->registerExceptionHandler();
-            $errorHandler->registerErrorHandler();
-            $errorHandler->registerShutdownFunction();
+
+            register_shutdown_function(array($this, 'setErrorHandler'));
         }
     }
 
@@ -24,8 +26,73 @@ class Module_Sentry_Service_Writer extends Zend_Log_Writer_Abstract
         return new self($config);
     }
 
+    public function setErrorHandler()
+    {
+        if (self::$message) {
+
+            if (!class_exists('Raven_Stacktrace')) {
+                spl_autoload_call('Raven_Stacktrace');
+            }
+
+            $message = self::$message['message'];
+            $stack = array();
+
+            if (class_exists('Error', false)) {
+                $backtrace = self::$message['backtrace'];
+                foreach ($backtrace as $file) {
+                    if (isset($file['function']) && $file['function'] == 'handleException' && isset($file['args'])) {
+                        foreach ($file['args'] as $item) {
+                            if ($item instanceof Error) {
+                                $message = $item->getMessage();
+                                $trace = array_merge(array(array(
+                                    'file' => $item->getFile(),
+                                    'line' => $item->getLine()
+                                )), $item->getTrace());
+
+                                $stack = array(
+                                    'frames' => Raven_Stacktrace::get_stack_info($trace)
+                                );
+
+                                break 1;
+                            }
+                        }
+                    }
+                }
+
+                if (isset(self::$message['exception'])) {
+                    $e = self::$message['exception'];
+                    if ($e instanceof Exception) {
+                        $trace = array_merge(array(array(
+                            'file' => $e->getFile(),
+                            'line' => $e->getLine()
+                        )), $e->getTrace());
+
+                        $stack = array(
+                            'frames' => Raven_Stacktrace::get_stack_info($trace)
+                        );
+                    }
+                }
+            }
+
+            self::$sentry->captureMessage($message, array(), array(
+                'stacktrace' => $stack
+            ));
+            //  self::$sentry->sendUnsentErrors();
+        }
+    }
+
     protected function _write($event)
     {
+        $lines = explode("\n", trim($event['message']));
+        if (preg_match('/^Error Code: (.*?)$/i', $lines[0])) {
+            unset($lines[0]);
+        }
 
+        $message = implode("\n", $lines);
+        self::$message = array(
+            'message' => $message,
+            'backtrace' => debug_backtrace(),
+            'exception' => (isset($event['exception']) ? $event['exception'] : false)
+        );
     }
 }
