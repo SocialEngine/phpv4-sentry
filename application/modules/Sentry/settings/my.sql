5e01fe6f1352e1260dc562e3168f85577769153b
RaymondBenc <me@raybenc.com>
2017-03-10 01:02:35 +0100

    Removing INSERT of version on module table

--- application/modules/Sentry/settings/my.sql
+++ application/modules/Sentry/settings/my.sql
@@ -1,11 +1,7 @@
 
-INSERT INTO `engine4_core_modules` (`name`, `title`, `description`, `version`, `enabled`, `type`) VALUES
-('Sentry', 'Sentry', 'Sentry', '1.0.0', 1, 'extra');
-
 INSERT IGNORE INTO `engine4_core_settings` (`name` , `value`) VALUES
 ('sentry.enabled', '0'),
 ('sentry.dsn', '0');
 
 INSERT IGNORE INTO `engine4_core_menuitems` (`name`, `module`, `label`, `plugin`, `params`, `menu`, `submenu`, `order`) VALUES
-
 ('core_admin_main_Sentry', 'Sentry', 'Sentry Integration', '', '{"route":"admin_default","module":"Sentry","controller":"settings"}', 'core_admin_main_settings', '', 999);
